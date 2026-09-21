d8cc7d05ddf583f16ff3759d5c6c5d377f171366
MrRimuru-glitch <heisrimuru661@gmail.com>
2026-09-21 08:33:53 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -4,18 +4,21 @@ async function getPairCode(phoneNumber) {
     const { state, saveCreds } = await useMultiFileAuthState('./auth_'+phoneNumber);
     const sock = makeWASocket({
         logger: pino({ level: 'silent' }),
-        auth: { creds: state.creds, keys: makeCacheableSignalKeyStore(state.keys, pino({ level: 'silent' })) },
+        auth: {
+            creds: state.creds,
+            keys: makeCacheableSignalKeyStore(state.keys, pino({ level: 'silent' }))
+        },
         printQRInTerminal: false,
-        browser: ["Rimuru Bot", "Chrome", "1.0"]
+        browser: ["Rimuru Bot", "Chrome", "1.0"],
+        connectTimeoutMs: 60000
     });
     sock.ev.on('creds.update', saveCreds);
-    if (!sock.authState.creds.registered) {
-        const cleanNumber = phoneNumber.replace(/\D/g,'');
-        await new Promise(r => setTimeout(r, 2000));
-        const code = await sock.requestPairingCode(cleanNumber);
-        return code;
-    } else {
-        return "ALREADY_PAIRED";
-    }
+    // Keep alive for 90 seconds so you have time to enter code
+    await new Promise(r => setTimeout(r, 3000));
+    const cleanNumber = phoneNumber.replace(/\D/g,'');
+    const code = await sock.requestPairingCode(cleanNumber);
+    // Don't close socket, let it wait for pairing
+    setTimeout(()=>{ try{sock.end()}catch(e){} }, 90000);
+    return code;
 }
 module.exports = { getPairCode };
